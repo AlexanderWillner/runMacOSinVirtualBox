2776aad90b6c6a747b532916ab007d990dff8fc6
Vitaly Samoylik <vitalys@gmail.com>
2020-08-18 10:11:04 +0300

    Increase installer volume size to 16Gb
    BigSur requires about 16Gb for installer

--- runMacOSVirtualbox.sh
+++ runMacOSVirtualbox.sh
@@ -163,7 +163,7 @@ createImage() {
     result "."
     ejectAll
     mkdir -p "$DST_DIR"
-    hdiutil create -o "$DST_DMG" -size 10g -layout SPUD -fs HFS+J &&
+    hdiutil create -o "$DST_DMG" -size 16g -layout SPUD -fs HFS+J &&
     hdiutil attach "$DST_DMG" -mountpoint "$DST_VOL" &&
     sudo "$INST_BIN" --nointeraction --volume "$DST_VOL" --applicationpath "$INST_VER" ||
     error "Could create or run installer. Please look in the log file..."
